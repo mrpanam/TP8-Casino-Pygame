bff00b66f001dc87ea277e1e82fceef7e9fef77b
mrpan <mrpanam.p@gmail.com>
2020-10-07 15:44:01 +0200

    TP8 Pygame Casino POO

--- Emplacements.py
+++ Emplacements.py
@@ -5,7 +5,7 @@ class Emplacement(pygame.sprite.Sprite):
 
     def __init__(self,pos_x,pos_y):
         super().__init__()
-        self.image= pygame.image.load("assets/orange.png")
+        self.image= pygame.image.load("assets/dollar.png")
         self.rect= self.image.get_rect()
         self.rect.x=pos_x
         self.rect.y=pos_y
--- Fruits.py
+++ Fruits.py
@@ -1,6 +1,5 @@
 import pygame
 
-
 class Fruit:
 
     def __init__(self, nom, gain, proba, img):
